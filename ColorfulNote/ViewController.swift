245a77599bead1a5b6df3a9ab187c34109a31947
Takanori Matsumoto <matscube@gmail.com>
2015-01-08 02:09:17 +0900

    Swipe

--- ColorfulNote/ViewController.swift
+++ ColorfulNote/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         blueView.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 0.9)
         view.addSubview(blueView)
         
+        let gesture = UISwipeGestureRecognizer()
+        
     }
 
     override func didReceiveMemoryWarning() {
